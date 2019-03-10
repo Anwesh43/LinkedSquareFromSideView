2168408041f71673228676bbc47c79b95eddc954
Anwesh43 <anweshthecool0@gmail.com>
2019-03-11 01:30:03 +0530

    using renderer in view

--- squarefromsideview/src/main/java/com/anwesh/uiprojects/squarefromsideview/SquareFromSideView.kt
+++ squarefromsideview/src/main/java/com/anwesh/uiprojects/squarefromsideview/SquareFromSideView.kt
@@ -53,16 +53,18 @@ fun Canvas.drawSFSNode(i : Int, scale : Float, paint : Paint) {
 
 class SquareFromSideView(ctx : Context) : View(ctx) {
 
+    private val renderer : Renderer = Renderer(this)
+
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
