755ae38580b5e8bb822a6ace93beb511bedfc1fb
Anwesh43 <anweshthecool0@gmail.com>
2019-03-11 01:15:34 +0530

    adding drawSFSNode canvas extension method

--- squarefromsideview/src/main/java/com/anwesh/uiprojects/squarefromsideview/SquareFromSideView.kt
+++ squarefromsideview/src/main/java/com/anwesh/uiprojects/squarefromsideview/SquareFromSideView.kt
@@ -17,8 +17,7 @@ val nodes : Int = 5
 val squares : Int = 4
 val scGap : Float = 0.05f
 val scDiv : Double = 0.51
-val sizeFactor : Int = 90
-val strokeFactor : Float = 2.9f
+val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#4527A0")
 val backColor : Int = Color.parseColor("#BDBDBD")
 
@@ -28,3 +27,26 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+fun Int.sf() : Float = 1f - 2 * this
+
+fun Canvas.drawSFSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    val hGap : Float = size / (squares)
+    save()
+    translate(w / 2, hGap * (i + 1))
+    rotate(90f * sc2)
+    for (j in 0..(squares - 1)) {
+        val sc : Float = sc1.divideScale(j, 2)
+        save()
+        translate((w/ 2 + hGap / 2) * j.sf() * sc, size - hGap * (j + 1))
+        drawRect(RectF(-hGap / 2, 0f, hGap / 2, hGap), paint)
+        restore()
+    }
+    restore()
+}
