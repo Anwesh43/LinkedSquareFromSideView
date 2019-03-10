57495fa829b08b74bfde9413e477b0d486fc6651
Anwesh43 <anweshthecool0@gmail.com>
2019-03-11 01:06:05 +0530

    adding constants and importing libraries

--- squarefromsideview/src/main/java/com/anwesh/uiprojects/squarefromsideview/SquareFromSideView.kt
+++ squarefromsideview/src/main/java/com/anwesh/uiprojects/squarefromsideview/SquareFromSideView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.squarefromsideview
  * Created by anweshmishra on 11/03/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val squares : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Int = 90
+val strokeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4527A0")
+val backColor : Int = Color.parseColor("#BDBDBD")
